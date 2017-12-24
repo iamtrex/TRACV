d352b02ee49ffd8aaea0d510474b2b4effd1a19d
RWEQX <rexlin54@yahoo.ca>
2017-12-23 23:04:50 -0800

    Updated Learning
    Signed-off-by: RWEQX <rexlin54@yahoo.ca>

---
+++ src/learning/JavaIntro/LearnJava.java
@@ -0,0 +1,144 @@
+package learning.JavaIntro;
+
+//Welcome to Learning Java!
+
+//In Java, code is stored in different files known as 'classes'
+//They are the .java files you see.
+
+//As you can probably tell, two slashes denote comments
+/*
+And these slashes allow us to write
+multi-line
+comments
+between them
+ */
+
+//To get started, we look at the declaration of the class:
+//We are in the class named LearnJava.
+//Note that in all classes, they are denoted like this:
+//  public class ClassName {
+//   //Some code goes here.
+//  }
+//In Java, we use the curly brackets {} to denote blocks of code.
+
+public class LearnJava {
+
+    //In Java, we declare variables with a type, and that type cannot just arbitrarily change.
+    //For example here we have created a variable of type "String", and we named that variable text.
+    //Note we also created the string with a value of Hello World.
+
+    String text = new String("Hello World");
+
+    //Note that String in this case is an OBJECT. Objects are the core of how Java programming works.
+    //With the exception of the very basic variable types: int, float, double, boolean, etc. All variables are
+    //always objects.
+    //In this case we created text as a new String, and gave it the parameter "Hello World". We will see that
+    //  this gives it a value "Hello World"
+
+    //Objects in java are creaetd in the form of:
+    //Object objName = new Object(Param1, Param2, ...); //Note you can have 0 parameters (@main function)
+
+    //A more common shorthand is to just use:
+    // String text = "Hello World". This is an exception for Strings only!
+
+    //This is an example of a method (aka. function).
+    //The method is called printTextToScreen.
+    //Methods are in the form of :
+    //public RETURN_TYPE functionName(VarType varName, Var2Type var2Name...){
+    // ... Code here
+    //}
+    public void printTextToScreen(){
+
+        //This just prints to the console. In this case, it prints the value of the String named text that we created
+        //  earlier;
+
+        System.out.println(text);
+        //This means that whenever we call the "printTextToScreen()" function, it will write the value of text.
+    }
+
+    //Here is a function that takes parameters, as you can see, it takes 2 integers, named a and b.
+    //We also see that the return type is int instead of void. This means we must use the return statement in the functeion
+    //Here we have return a+b;, therefore this function just adds the value of a and b and returns it.
+    public int addNumbers(int a, int b){
+        return a+b;
+    }
+
+
+    //Here we see a function with NO RETURN STATEMENT! It also seems to have the same name as the File (LearnJava)
+    //This is known as the CONSTRUCTOR.
+    //Constructors are functions that automatically run when an object of the class is made.
+    //  They "Construct" the class...
+    //Remember how we created String with a parameter "New World", that means in the String class we have something like:
+    //public String(Parameter1 word){
+    // ... Create a string with value = word...
+    //}
+
+    public LearnJava(){
+        printTextToScreen(); //So here you see we run the print to screen function, this will print out the value of text
+                //The value of text hasn't changed so it's still "Hello World"
+        int a = 4;
+        int b = 5; //Note that here we create what is known as temporary variables.
+        //These variables belong to the function, so other functions will NOT be able to see or access these variables
+
+
+        //We can both pass direct numbers and variables because they are all integers. or a mix.
+        int c = addNumbers(1, 5);
+        int d = addNumbers(a, b);
+        int e = addNumbers(c, -1);
+
+        //NOte that we set the value of c to be the return value of addNumbers(1, 5); Looking back, we can see that
+            //This is obviously 6.
+        //e is then c + -1, and since c is 6, we know e = -1;
+
+        //Now we will create anotehr Object.
+        RandomNumberGenerator RNG = new RandomNumberGenerator();
+
+        //Since we now have an object of the class RandomNumberGenerator, we can use the functions in that classs.
+        //This is how Java gets different files to work together.
+        int f = RNG.getRandomNumber(5);
+        //Go to the RandomNumberGenerator file and see what the function getRandomNumber does ...
+        // (The name is pretty intuitive though.)
+
+
+        //F will tehrefore be a random int between 0 and 5;
+
+        //text = f; //<-- we see if we uncomment this line, it's an error. This is because you can't simply merge
+        //      Types in Java. text is a string, f is an integer. they don't just combine simply.
+
+        text = String.valueOf(f); //The function valueOf creates a string from an integer, so it effectively converts
+                    // f to be a string.
+
+        //Now note we changed the variable text to be something else, so now let's try printing it again:
+        printTextToScreen();
+
+        //We will see this time it prints a random number between 1 and 5.
+
+
+
+
+
+
+    }
+
+
+    //Here is a special method. The main method.
+    //Don't worry too much about the syntax, it's pretty much alwasy teh same.
+    //Effectively , when Java starts, it wants to run this method always,  a method in the form of
+    //public static void main(String args[]){
+    // ...
+    // }
+    //So we just need to include it.
+    public static void main(String args[]){
+
+        //To start off our program, we create a LearnJava Object!
+        //Note Learnjava is this class! But by creating an object we are able to run the constructor.
+        //  So the logic we just had before now initializes and executes.
+        LearnJava LJ = new LearnJava();
+
+        LJ.printTextToScreen(); //You see we can also access methods in this class this way :P
+        //Now note, what does this line print?
+        //So does it execute after the constructor changed text to be a number or is it still "Hello World" ?
+
+
+    }
+}
--- src/learning/Main.java
+++ src/learning/Main.java
@@ -1,18 +1,53 @@
 package learning;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+/**
+ * A quick intro to Java.
+ */
+
+//Here is the declaration of the class's name. We can ignore the extends Application right now.
+    // Classes are just individual files in Java. We will see how we can communicate between different classes
+    //  using objects below.
+
+//It should be noted that classes are declared with the followingn syntax:
+    //public class ClassName{
+    //    variables and
+    //    code go here...
+    //} <- note we use curly brackets to start and close blocks of code. Note we use the same for each method (function)
 public class Main extends Application {
 
+
+    //In Java, we must specify what type of variable variables are. In this case we create a variable of type
+    //     "Group".
+    //In Java, besides the basic variables (boolean, int, double, float, etc.) all variables are OBJECTS
+    // So Group is a TYPE of OBJECT.
+    // And we have created a variable root of type Group.
+    private Group root;
+
+
+    //we can ignore the @Override annotation for now.
+    //This below is a METHOD (you may know them as functions).
+    //This method is named "start", and takes 1 parameter (Stage primaryStage).
+    //The parameter is therefore a variable of type Stage.
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Creates a basic button and stores that to the variable named 'btn'
+        //Note that Button, Stage, Group are built in, if you scroll to the top, you shoudl see that we import
+        //  these because they are part of what we call the "Java Library." Aka it's a lot of code people already
+        //  wrote for us that we can borrow to make coding faster.
 
+        //In this case, someone wrote a "Button" class. So when we create a Button, we are creating a variable of that
+        //   type and we can access it.
+        Button btn = new Button("This is the button's text");
+
+        new Group();
+        root.getChildren().add(btn);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
