155cd9faf8d8de1fc9d0547a04d52a2991c9ade3
RWEQX <rexlin54@yahoo.ca>
2017-12-25 00:54:02 -0800

    Observer Pattern
    Signed-off-by: RWEQX <rexlin54@yahoo.ca>

--- src/com/tracv/util/Comp.java
+++ src/com/tracv/util/Comp.java
@@ -26,4 +26,31 @@ public class Comp {
 
         addTo.add(toAdd, c);
     }
+
+    public static void add(JComponent toAdd, JComponent addTo) {
+        add(toAdd, addTo, 0, 0, 1, 1, 1, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER);
+    }
+
+    public static void add(JComponent toAdd, JLayeredPane addTo, int i) {
+        add(toAdd, addTo, i, 0, 0, 1, 1, 1, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER);
+    }
+    public static void add(JComponent toAdd, JLayeredPane addTo, int i, int x, int y, int width, int height,
+                           double weightx, double weighty, int fill, int anchor) {
+        if (!(addTo.getLayout() instanceof GridBagLayout)) {
+            addTo.setLayout(new GridBagLayout());
+        }
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridwidth = width;
+        c.gridheight = height;
+        c.fill = fill;
+        c.anchor = anchor;
+        c.gridx = x;
+        c.gridy = y;
+        c.weightx = weightx;
+        c.weighty = weighty;
+
+        addTo.setLayer(toAdd, i);
+        addTo.add(toAdd, c, i);
+    }
 }
