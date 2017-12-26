64a046efc0f166e3a5cf3e90eaa21f45383b4a9a
RWEQX <rexlin54@yahoo.ca>
2017-12-25 16:54:34 -0800

    Updated Menu - Foggy effect.
    Made basic menu with single button for transition.
    Menu creates a basic foggy effect to blur out the background :P
    
    Signed-off-by: RWEQX <rexlin54@yahoo.ca>

--- src/com/tracv/ui/MenuPane.java
+++ src/com/tracv/ui/MenuPane.java
@@ -1,27 +1,111 @@
 package com.tracv.ui;
 
+import com.tracv.swing.Button;
+
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
 
 /*
- * Represents a Menu Overlay. 
+ * Represents a Menu Overlay.
  */
-public class MenuPane extends JPanel {
+public class MenuPane extends JPanel implements ActionListener {
 
+    private TDFrame tdf;
 
-    public MenuPane(){
-        this.setBackground(new Color(0, 0, 0, 0.5f));
-        this.setOpaque(false);
+    Button returnToMain = new Button("Return to Main");
 
-        this.setBorder(new LineBorder(Color.RED, 3));
+    public MenuPane(TDFrame tdf) {
+        this.tdf = tdf;
+        //    this.setBorder(new LineBorder(Color.RED, 3));
+
+        this.addMouseListener(new MyMouseListener());
+
+        this.add(returnToMain);
+        returnToMain.addActionListener(this);
 
 
     }
 
-    @Override
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
+    private BufferedImage background;
 
+    @Override
+    public void setVisible(boolean b){
+        if(b){
+            createBlurredImage();
+        }
+
+        super.setVisible(b);
+    }
+
+    private void createBlurredImage(){
+        try {
+            Robot robot = new Robot();
+            BufferedImage image = robot.createScreenCapture(new Rectangle(
+                    this.getX(), this.getY(), this.getWidth(), this.getHeight()));
+
+            Kernel kernel = new Kernel(3, 3, new float[]{1f / 9f, 1f / 9f, 1f / 9f,
+                    1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f});
+            BufferedImageOp op = new ConvolveOp(kernel);
+            background = op.filter(image, null);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if(background != null) {
+            g.drawImage(background, 0, 0, null);
+        }
+        g.setColor(new Color(238,242,245,200));
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (source == returnToMain) {
+            tdf.switchToMainPanel();
+        }
+    }
+
+
+    private class MyMouseListener implements MouseListener {
+        boolean down = false;
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            down = true;
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            if (down) {
+                tdf.toggleMenu(false);
+                down = false;
+            }
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+        }
     }
 }
