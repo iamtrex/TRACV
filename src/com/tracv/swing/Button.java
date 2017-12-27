44b636705fd4eeec5321014e627e0e1c1a3902e5
RWEQX <rexlin54@yahoo.ca>
2017-12-26 19:30:27 -0800

    Made pretty looking buttons
    Signed-off-by: RWEQX <rexlin54@yahoo.ca>

--- src/com/tracv/swing/Button.java
+++ src/com/tracv/swing/Button.java
@@ -5,6 +5,8 @@ import com.tracv.util.Constants;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 /*
     Customized version of JButton to keep with design.
@@ -23,12 +25,66 @@ public class Button extends JButton {
 
     private void setLook() {
         this.setContentAreaFilled(false);
-        //this.setBorderPainted(false);
+        this.setBorderPainted(false);
+
         this.setFont(Constants.DEFAULT_FONT);
+        this.addMouseListener(new MyMouseListener());
 
-        //TODO remove testing:
-        //this.setBorder(new LineBorder(Color.BLACK, 1));
+        //TODO make proper selected border.
+        this.setBorder(new LineBorder(Color.BLACK, 1));
+    }
 
+    @Override
+    protected void paintComponent(Graphics g){
+        if(this.isSelected()){
+            g.setColor(Constants.BUTTON_SELECTED_COLOR);
+        }else{
+            g.setColor(Constants.BUTTON_UNSELECTED_COLOR);
+        }
+
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+        super.paintComponent(g);
+
+    }
+
+    private class MyMouseListener implements MouseListener {
+
+        private void refresh(){
+            SwingUtilities.invokeLater(()->Button.this.validate());
+        }
+        private void paintBorder(boolean b){
+            if(Button.this.isBorderPainted() != b) {
+                Button.this.setBorderPainted(b);
+                refresh();
+            }
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            paintBorder(false);
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            Button.this.setSelected(true);
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            Button.this.setSelected(false);
+            paintBorder(false);
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+           paintBorder(true);
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            paintBorder(false);
+        }
     }
 }
 
