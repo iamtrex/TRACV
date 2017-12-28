d7aeb2a3ee9a531dc68a2ad0eeed999d38a70630
zestiax <charles.miffy@gmail.com>
2017-12-28 15:31:09 -0800

    Added JavaDoc to Base, GameMap and Tower, implemented simple draw method for Base and Tower

--- src/com/tracv/model/Base.java
+++ src/com/tracv/model/Base.java
@@ -1,17 +1,41 @@
 package com.tracv.model;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 
+/**
+ * Represents the Base on the Game Map
+ * The base should exist on the GameMap and its health value should be greater than 0
+ */
 public class Base extends GameComponent{
     private int health;
+    private int size;
 
-    public Base(int health, double x, double y, String iconPath) {
+    /**
+     * Constructor for the Base
+     * @param health the health value of the base
+     * @param size the size of the base, width = height = size
+     * @param x the x coordinate of the base
+     * @param y the y coordinate of the base
+     * @param iconPath the directory of the base's icon
+     */
+    public Base(int health, int size, double x, double y, String iconPath) {
         super(x, y, iconPath);
         this.health = health;
+        this.size = size;
     }
 
+    /**
+     *CURRENTLY DRAWS A CENTERED BLUE CIRCLE WITH SIZE EQUALS TO size
+     * @param g Swing Graphics
+     */
     public void draw(Graphics g) {
-
+        g.setColor(Color.BLUE);
+        Graphics2D g2d = (Graphics2D)g;
+        double centerX = x - (size/2);
+        double centerY = y - (size/2);
+        Ellipse2D.Double circle = new Ellipse2D.Double(centerX, centerY, size, size);
+        g2d.fill(circle);
     }
 
     public int getHealth() {
--- src/com/tracv/model/Tower.java
+++ src/com/tracv/model/Tower.java
@@ -3,6 +3,10 @@ package com.tracv.model;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
 
+/**
+ * Represents a Tower on the GameMap
+ * No fields should be null, atkDmg, fireRate, cost, size and range should be greater than 0
+ */
 public class Tower extends GameComponent {
     private int atkDmg;
     private double fireRate;
@@ -10,6 +14,17 @@ public class Tower extends GameComponent {
     private int size;
     private int range;
 
+    /**
+     * Constructor for a Tower
+     * @param atkDmg the attack damage value of the tower
+     * @param fireRate the fire rate of the tower
+     * @param cost the amount the tower costs to construct
+     * @param size the width and height of the tower, the tower is a square (width = height)
+     * @param range the attack range of the tower
+     * @param x the x coordinate of the tower's position
+     * @param y the y coordinate of the tower's position
+     * @param iconPath directory of the tower's icon
+     */
     public Tower(int atkDmg, double fireRate, int cost, int size, int range, double x, double y, String iconPath) {
         super(x, y, iconPath);
         this.atkDmg = atkDmg;
@@ -19,12 +34,16 @@ public class Tower extends GameComponent {
         this.range = range;
     }
 
+    /**
+     * CURRENTLY DRAWS A CENTERED RED CIRCLE WITH SIZE EQUALS TO size
+     * @param g Swing Graphics
+     */
     public void draw(Graphics g) {
         g.setColor(Color.RED);
         Graphics2D g2d = (Graphics2D)g;
-        double centerX = x - (1/2);
-        double centerY = y - (1/2);
-        Ellipse2D.Double circle = new Ellipse2D.Double(centerX, centerY, 1, 1);
+        double centerX = x - (size/2);
+        double centerY = y - (size/2);
+        Ellipse2D.Double circle = new Ellipse2D.Double(centerX, centerY, size, size);
         g2d.fill(circle);
     }
 
