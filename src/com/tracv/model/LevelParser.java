8b7387babcd34b54a38ce4ce9e483e52f1cfde03
RWEQX <rexlin54@yahoo.ca>
2018-03-05 23:31:45 -0800

    Working on Level Select Screen
    Signed-off-by: RWEQX <rexlin54@yahoo.ca>

---
+++ src/com/tracv/model/LevelParser.java
@@ -0,0 +1,10 @@
+package com.tracv.model;
+
+import java.util.List;
+
+public class LevelParser {
+    public static List<String> parseLevels() {
+
+
+    }
+}
---
+++ src/com/tracv/ui/LevelSelectButton.java
@@ -0,0 +1,14 @@
+package com.tracv.ui;
+
+import com.tracv.swing.Button;
+
+public class LevelSelectButton extends Button {
+    public LevelSelectButton(String s){
+        super();
+        this.setText(s);
+
+
+
+    }
+
+}
